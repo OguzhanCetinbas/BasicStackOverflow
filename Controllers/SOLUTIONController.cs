05c8d4dc915cac5267150deb441a8ec9fc9c2819
OguzhanCetinbas <49721458+OguzhanCetinbas@users.noreply.github.com>
2022-02-08 18:08:49 +0300

    Add files via upload

---
+++ Controllers/SOLUTIONController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Raklet.Models.EntityDataModel;
+
+namespace Raklet.Controllers
+{
+    public class SOLUTIONController : Controller
+    {
+        private StackOverflowEntities db = new StackOverflowEntities();
+
+        // GET: SOLUTION
+        public ActionResult Index()
+        {
+            var sOLUTION = db.SOLUTION.Include(s => s.SUBJECT);
+            return View(sOLUTION.ToList());
+        }
+
+        // GET: SOLUTION/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SOLUTION sOLUTION = db.SOLUTION.Find(id);
+            if (sOLUTION == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sOLUTION);
+        }
+
+        // GET: SOLUTION/Create
+        public ActionResult Create()
+        {
+            ViewBag.SUBID = new SelectList(db.SUBJECT, "SUBJECT_ID", "PROBLEM");
+            return View();
+        }
+
+        // POST: SOLUTION/Create 
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SOLUTION_ID,NAME_SURENAME,E_MAIL,SOLUTION1,SUBID")] SOLUTION sOLUTION)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SOLUTION.Add(sOLUTION);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.SUBID = new SelectList(db.SUBJECT, "SUBJECT_ID", "PROBLEM", sOLUTION.SUBID);
+            return View(sOLUTION);
+        }
+
+        // GET: SOLUTION/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SOLUTION sOLUTION = db.SOLUTION.Find(id);
+            if (sOLUTION == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.SUBID = new SelectList(db.SUBJECT, "SUBJECT_ID", "PROBLEM", sOLUTION.SUBID);
+            return View(sOLUTION);
+        }
+
+        // POST: SOLUTION/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SOLUTION_ID,NAME_SURENAME,E_MAIL,SOLUTION1,SUBID")] SOLUTION sOLUTION)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sOLUTION).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.SUBID = new SelectList(db.SUBJECT, "SUBJECT_ID", "PROBLEM", sOLUTION.SUBID);
+            return View(sOLUTION);
+        }
+
+        // GET: SOLUTION/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SOLUTION sOLUTION = db.SOLUTION.Find(id);
+            if (sOLUTION == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sOLUTION);
+        }
+
+        // POST: SOLUTION/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SOLUTION sOLUTION = db.SOLUTION.Find(id);
+            db.SOLUTION.Remove(sOLUTION);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/StackOverflowController.cs
@@ -0,0 +1,61 @@
+﻿using Raklet.Models.EntityDataModel;
+using Raklet.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Raklet.Controllers
+{
+    public class StackOverflowController : Controller
+    {
+        StackOverflowEntities db = new StackOverflowEntities();
+
+        public ActionResult Index() {
+
+            return View(db.SUBJECT.ToList());
+        }
+        public ActionResult Create() {
+
+
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Create(string problem) {
+
+            SUBJECT s = new SUBJECT();
+            s.PROBLEM = problem;
+            db.SUBJECT.Add(s);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id) {
+            List<SOLUTION> value = db.SOLUTION.Where(x => x.SUBID == id).ToList();
+            return View(value.ToList());
+        }
+        public ActionResult Search(string q)
+        {
+            var value = from d in db.SUBJECT select d;
+            if (!string.IsNullOrEmpty("q")) {
+                value = value.Where(x => x.PROBLEM.Contains(q));
+            }
+            return View(value.ToList());
+        }
+
+        //[HttpPost]
+
+        //public ActionResult CreateDetail(SOLUTION solution)
+        //{
+        //    db.SOLUTION.Add(solution);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Details");
+
+
+        //    return View();
+
+        //}
+    }
+    }
